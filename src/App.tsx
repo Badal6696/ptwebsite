cf7aa772e319736ca93573087bd9f1d807163a67
Badal6696 <151741515+Badal6696@users.noreply.github.com>
2026-03-17 11:58:00 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
+import Booking from './pages/Booking';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="booking" element={<Booking />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
